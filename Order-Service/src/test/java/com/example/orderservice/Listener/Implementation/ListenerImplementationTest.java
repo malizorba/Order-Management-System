138b4baae817e982f739da8a4cc822b285e29804
malizorba <mali.zorba@hotmail.com>
2025-09-11 23:24:20 +0300

    listener unit test eklendi

--- Order-Service/src/test/java/com/example/orderservice/Listener/Implementation/ListenerImplementationTest.java
+++ Order-Service/src/test/java/com/example/orderservice/Listener/Implementation/ListenerImplementationTest.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(MockitoExtension.class)
 class ListenerImplementationTest {
     @Mock
@@ -38,30 +39,31 @@ class ListenerImplementationTest {
         String message = "{\"orderId\":\"123e4567-e89b-12d3-a456-426614174000\"}";
 
         // Burada PaymentFailedEvent sınıfını kullanarak bir mock nesne oluşturabilirsiniz
-         PaymentFailedEvent event = new PaymentFailedEvent(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"),"123");
+        PaymentFailedEvent event = new PaymentFailedEvent(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"), "123");
 
         // ObjectMapper'ın readValue metodunu mock'layın
-         Mockito.when(objectMapper.readValue(message, PaymentFailedEvent.class)).thenReturn(event);
+        Mockito.when(objectMapper.readValue(message, PaymentFailedEvent.class)).thenReturn(event);
 
         // orderService.cancelOrder metodunun çağrıldığını doğrulayın
-         listenerImplementation.consume(message);
-         Mockito.verify(orderService).cancelOrder(event.getOrderId());
+        listenerImplementation.consume(message);
+        Mockito.verify(orderService).cancelOrder(event.getOrderId());
 
     }
+
     @Test
     void consume_orderPaymentEvent_success() throws Exception {
         String message = "{\"orderId\":\"123e4567-e89b-12d3-a456-426614174000\"}";
-        OrderPaymentEvent event = new OrderPaymentEvent(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"),"123");
+        OrderPaymentEvent event = new OrderPaymentEvent(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"), "123");
 
         Mockito.when(objectMapper.readValue(message, OrderPaymentEvent.class)).thenReturn(event);
 
         listenerImplementation.consumeOrderCompleted(message);
-        Mockito.verify(orderService,Mockito.times(1)).completeOrder(event.getOrderId());
+        Mockito.verify(orderService, Mockito.times(1)).completeOrder(event.getOrderId());
     }
 
     @Test
     void consume_invalidJson_shouldNotCrash() {
-        String invalidMessage="invalid-json";
+        String invalidMessage = "invalid-json";
 
         listenerImplementation.consume(invalidMessage);
 
